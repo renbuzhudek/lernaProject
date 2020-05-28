05ea862ee5ca1f4b51963e491037410304bfa250
郑治益 <464879673@qq.com>
2020-05-28 20:28:45 +0800

    c

--- packages/module-a/lib/module-a.js
+++ packages/module-a/lib/module-a.js
@@ -4,11 +4,12 @@ module.exports = moduleA;
 
 
 const moduleB = require('module-b');
-
+const moduleC = require('module-c');
 var moduleA = function() {
     console.log(moduleB());
 }
 
 moduleA()
+moduleC()
 
 // 优秀
